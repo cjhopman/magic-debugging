68d8f56e08cba444398a4ce2701b89e14540071a
Chris Hopman <cjhopman@google.com>
2013-11-01 18:30:34 -0700

    Apply some clang-format

--- enable_if.h
+++ enable_if.h
@@ -18,12 +18,18 @@ struct extract_type<void(T)> { typedef T type; };
 
 #define EXTRACT_TYPE(t) extract_type<void(t)>::type
 
-template <typename T> \
-struct maker { static const T& make(); }; \
-template <typename T> \
-struct maker<T[]> { static T* make(); }; \
-template <typename T, size_t n> \
-struct maker<T[n]> { static T* make(); }; \
+template <typename T>
+struct maker {
+  static const T& make();
+};
+template <typename T>
+struct maker<T[]> {
+  static T* make();
+};
+template <typename T, size_t n>
+struct maker<T[n]> {
+  static T* make();
+};
 
 template <typename R, typename Args>
 struct FunctionTypeImpl {
@@ -35,19 +41,19 @@ template <typename>
 struct FunctionType {};
 
 template <typename R>
-struct FunctionType<R(*)()> {
+struct FunctionType<R (*)()> {
   typedef R ReturnType;
   typedef MAGIC_TYPE_LIST() ArgsTypes;
 };
 
 template <typename R, typename A0>
-struct FunctionType<R(*)(A0)> {
+struct FunctionType<R (*)(A0)> {
   typedef R ReturnType;
   typedef MAGIC_TYPE_LIST(A0) ArgsTypes;
 };
 
 template <typename R, typename A0, typename A1>
-struct FunctionType<R(*)(A0, A1)> {
+struct FunctionType<R (*)(A0, A1)> {
   typedef R ReturnType;
   typedef MAGIC_TYPE_LIST(A0, A1) ArgsTypes;
 };
@@ -63,47 +69,46 @@ template <typename>
 struct MemberFunctionType {};
 
 template <typename R, typename C>
-struct MemberFunctionType<R(C::*)()> {
+struct MemberFunctionType<R (C::*)()> {
   typedef R ReturnType;
   typedef C ClassType;
   typedef MAGIC_TYPE_LIST() ArgsTypes;
 };
 
 template <typename R, typename C, typename A0>
-struct MemberFunctionType<R(C::*)(A0)> {
+struct MemberFunctionType<R (C::*)(A0)> {
   typedef R ReturnType;
   typedef C ClassType;
   typedef MAGIC_TYPE_LIST(A0) ArgsTypes;
 };
 
 template <typename R, typename C, typename A0, typename A1>
-struct MemberFunctionType<R(C::*)(A0, A1)> {
+struct MemberFunctionType<R (C::*)(A0, A1)> {
   typedef R ReturnType;
   typedef C ClassType;
   typedef MAGIC_TYPE_LIST(A0, A1) ArgsTypes;
 };
 
 template <typename R, typename C>
-struct MemberFunctionType<R(C::*)() const> {
+struct MemberFunctionType<R (C::*)() const> {
   typedef R ReturnType;
   typedef C ClassType;
   typedef MAGIC_TYPE_LIST() ArgsTypes;
 };
 
 template <typename R, typename C, typename A0>
-struct MemberFunctionType<R(C::*)(A0) const> {
+struct MemberFunctionType<R (C::*)(A0) const> {
   typedef R ReturnType;
   typedef C ClassType;
   typedef MAGIC_TYPE_LIST(A0) ArgsTypes;
 };
 
 template <typename R, typename C, typename A0, typename A1>
-struct MemberFunctionType<R(C::*)(A0, A1) const> {
+struct MemberFunctionType<R (C::*)(A0, A1) const> {
   typedef R ReturnType;
   typedef C ClassType;
   typedef MAGIC_TYPE_LIST(A0, A1) ArgsTypes;
 };
-
 
 template <int i, typename F>
 struct extract_argument {
@@ -111,63 +116,79 @@ struct extract_argument {
 };
 
 struct any_conversion {
-  template<class T>
+  template <class T>
   any_conversion(const T&);
 };
 
-#define DEFINE_HAS_MEM_FUNC(funcname, sig, name) \
-template <typename T> \
-struct name { \
-  typedef char yes[1]; \
-  typedef char no[2]; \
-  template <typename Base> struct func_sig { typedef typename EXTRACT_TYPE(sig) type; }; \
-  template <typename U, U> struct type_check; \
-  template <typename _1> static yes &chk(type_check<typename func_sig<typename MemberFunctionType<__typeof__(&_1::funcname)>::ClassType>::type, &_1::funcname> *); \
-  template <typename > static no &chk(...); \
-  typedef bool_<sizeof(chk<T>(0)) == sizeof(yes)> type; \
-};
+#define DEFINE_HAS_MEM_FUNC(funcname, sig, name)                     \
+  template <typename T>                                              \
+  struct name {                                                      \
+    typedef char yes[1];                                             \
+    typedef char no[2];                                              \
+    template <typename Base>                                         \
+    struct func_sig {                                                \
+      typedef typename EXTRACT_TYPE(sig) type;                       \
+    };                                                               \
+    template <typename U, U>                                         \
+    struct type_check;                                               \
+    template <typename _1>                                           \
+    static yes& chk(                                                 \
+        type_check<typename func_sig<typename MemberFunctionType<    \
+                       __typeof__(&_1::funcname)>::ClassType>::type, \
+                   &_1::funcname>*);                                 \
+    template <typename>                                              \
+    static no& chk(...);                                             \
+    typedef bool_<sizeof(chk<T>(0)) == sizeof(yes)> type;            \
+  };
 
-#define DEFINE_HAS_FUNC(funcname, sig, name) \
-namespace adl_barrier { \
-  typedef char no; \
-  struct yes { no dummy[2]; }; \
-  struct no_overload {}; \
-  static yes is_overloaded_impl(...) { return *(yes*)1; } \
-  static no is_overloaded_impl(const no_overload&) { return *(no*)1; } \
-  static no_overload funcname(any_conversion, any_conversion) { return no_overload(); } \
-  template<class T> \
-  struct name \
-  { \
-    typedef typename extract_argument<0, void(sig)>::type arg0_t; \
-    typedef typename extract_argument<1, void(sig)>::type arg1_t; \
-    enum B { value = \
-        sizeof(is_overloaded_impl(funcname(maker<arg0_t>::make(), maker<arg1_t>::make()))) == \
-        sizeof(yes) \
-    }; \
-    typedef name type; \
-  }; \
-} \
-using adl_barrier::name;
+#define DEFINE_HAS_FUNC(funcname, sig, name)                                  \
+  namespace adl_barrier {                                                     \
+  typedef char no;                                                            \
+  struct yes {                                                                \
+    no dummy[2];                                                              \
+  };                                                                          \
+  struct no_overload {};                                                      \
+  static yes is_overloaded_impl(...) { return *(yes*)1; }                     \
+  static no is_overloaded_impl(const no_overload&) { return *(no*)1; }        \
+  static no_overload funcname(any_conversion, any_conversion) {               \
+    return no_overload();                                                     \
+  }                                                                           \
+  template <class T>                                                          \
+  struct name {                                                               \
+    typedef typename extract_argument<0, void(sig)>::type arg0_t;             \
+    typedef typename extract_argument<1, void(sig)>::type arg1_t;             \
+    enum B {                                                                  \
+      value = sizeof(is_overloaded_impl(                                      \
+                  funcname(maker<arg0_t>::make(), maker<arg1_t>::make()))) == \
+              sizeof(yes)                                                     \
+    };                                                                        \
+    typedef name type;                                                        \
+  };                                                                          \
+  }                                                                           \
+  using adl_barrier::name;
 
+#define DEFINE_HAS_TYPEDEF(typedef_, name)               \
+  template <typename T>                                  \
+  struct name {                                          \
+    template <class U>                                   \
+    static char (&test(typename U::typedef_ const*))[1]; \
+    template <class U>                                   \
+    static char (&test(...))[2];                         \
+    enum B {                                             \
+      value = (sizeof(test<T>(0)) == 1)                  \
+    };                                                   \
+    typedef name type;                                   \
+  };                                                     \
+  template <typename T>                                  \
+  struct name##_c {                                      \
+    typedef bool_<name<T>::value> type;                  \
+  };
 
-#define DEFINE_HAS_TYPEDEF(typedef_, name) \
-template <typename T> \
-struct name { \
-  template<class U> static char (&test(typename U::typedef_ const*))[1]; \
-  template<class U> static char (&test(...))[2]; \
-  enum B { value = (sizeof(test<T>(0)) == 1) }; \
-  typedef name type; \
-}; \
-template <typename T> \
-struct name##_c { \
-  typedef bool_<name<T>::value> type; \
-};
-
-
-#define ENABLE_IF_TYPEDEF(value, name) \
-template <typename T> \
-struct name { typedef typename extract_type<void(value)>::type type; };
-
+#define ENABLE_IF_TYPEDEF(value, name)                     \
+  template <typename T>                                    \
+  struct name {                                            \
+    typedef typename extract_type<void(value)>::type type; \
+  };
 }
 
 #endif
