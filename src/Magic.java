4f35423584b435dbacc1456ccfc4ded88882a6a7
Chris Hopman <cjhopman@google.com>
2014-04-17 16:07:23 -0700

    .

---
+++ src/Magic.java
@@ -0,0 +1,19 @@
+package magic;
+
+public class Magic {
+    public static void trace(String s) {
+        int magicNumber = 3;
+        StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+        StackTraceElement caller = stack[magicNumber];
+        android.util.Log.e("MAGIC-Java", caller.getFileName() + ":" + caller.getLineNumber() + ":" + caller.getMethodName() + " " + s);
+    }
+
+    public static void dumpStack() {
+        int magicNumber = 3;
+        StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+        for (int i = magicNumber; i < stack.length; i++) {
+            StackTraceElement caller = stack[i];
+            android.util.Log.e("MAGIC-Java", caller.getFileName() + ":" + caller.getLineNumber() + ":" + caller.getMethodName());
+        }
+    }
+}
